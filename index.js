b1128d9534e2d14eb8657c6b7b6fd6606d85c9cb
Grok TuanDo <dodangtuan609@gmail.com>
2025-03-14 10:54:43 +0700

    add controller, service

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require("express");
+import express from 'express';
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World! Hieu Beo.");
+app.get('/', (req, res) => {
+  res.send('Hello World! Hieu Beo.');
 });
 
 app.listen(port, () => {
---
+++ src/services/sensor.service.js
@@ -0,0 +1,15 @@
+module.exports.getAll = async (req, res) => {
+  return 'get all';
+};
+module.exports.getOne = async (req, res) => {
+  return 'get one';
+};
+module.exports.create = async (req, res) => {
+  return 'create';
+};
+module.exports.update = async (req, res) => {
+  return 'update';
+};
+module.exports.delete = async (req, res) => {
+  return 'delete';
+};
